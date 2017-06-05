07e53fa194c746f510ce6bd6ceca9f7be940bc97
Shivan Modha <shivanmodha@gmail.com>
2017-06-05 16:40:50 -0400

    JSON Control Box
    Add or remove control buttons through window_config.json

--- main/window_main.js
+++ main/window_main.js
@@ -12,27 +12,27 @@ const toolbar_button_box = "☐";
 const toolbar_button_box_stretch = "⛶";
 function getLabel(string)
 {
-    if (string === "line")
+    if (string === "#line")
     {
         return toolbar_button_line;
     }
-    else if (string === "cross")
+    else if (string === "#cross")
     {
         return toolbar_button_cross;
     }
-    else if (string === "circle")
+    else if (string === "#circle")
     {
         return toolbar_button_circle;
     }
-    else if (string === "box")
+    else if (string === "#box")
     {
         return toolbar_button_box;
     }
-    else if (string === "box stretch")
+    else if (string === "#box-stretch")
     {
         return toolbar_button_box_stretch;
     }
-    return "";
+    return string;
 }
 /**
  * Window Elements
@@ -45,6 +45,8 @@ var element_titlebar_buttons;
 var element_titlebar_minimize;
 var element_titlebar_maximize;
 var element_titlebar_close;
+
+var element_titlebar_control_box = [10];
 function initialize_elements()
 {
     window_config = require('./resources/window_config.json');
@@ -54,34 +56,15 @@ function initialize_elements()
     element_titlebar_buttons = document.getElementById("titlebutton");
     
     var titlebuttonwidth = 0;
-    if (window_config.toolbar_close_show === true)
+    element_titlebar_control_box = [window_config.control_box.length];
+    for (var i = 0; i < window_config.control_box.length; i++)
     {
-        element_titlebar_close = document.createElement("button");
-        element_titlebar_close.setAttribute("class", "button");
-        element_titlebar_close.setAttribute("onclick", "event_titlebar_exit()");
-        element_titlebar_close.setAttribute("id", "tb-btn-exit");
-        element_titlebar_close.innerHTML = getLabel(window_config.toolbar_close_type);
-        element_titlebar_buttons.appendChild(element_titlebar_close);
-        titlebuttonwidth += 40;
-    }
-    if (window_config.toolbar_maximize_show === true)
-    {
-        element_titlebar_maximize = document.createElement("button");
-        element_titlebar_maximize.setAttribute("class", "button");
-        element_titlebar_maximize.setAttribute("onclick", "event_titlebar_maximize()");
-        element_titlebar_maximize.setAttribute("id", "tb-btn-max");
-        element_titlebar_maximize.innerHTML = getLabel(window_config.toolbar_maximize_type);
-        element_titlebar_buttons.appendChild(element_titlebar_maximize);
-        titlebuttonwidth += 40;
-    }
-    if (window_config.toolbar_minimize_show === true)
-    {
-        element_titlebar_minimize = document.createElement("button");
-        element_titlebar_minimize.setAttribute("class", "button");
-        element_titlebar_minimize.setAttribute("onclick", "event_titlebar_minimize()");
-        element_titlebar_minimize.setAttribute("id", "tb-btn-min");
-        element_titlebar_minimize.innerHTML = getLabel(window_config.toolbar_minimize_type);
-        element_titlebar_buttons.appendChild(element_titlebar_minimize);
+        element_titlebar_control_box[i] = document.createElement("button");
+        element_titlebar_control_box[i].setAttribute("class", "button");
+        element_titlebar_control_box[i].setAttribute("onclick", window_config.control_box[i].click);
+        element_titlebar_control_box[i].setAttribute("id", window_config.control_box[i].type);
+        element_titlebar_control_box[i].innerHTML = getLabel(window_config.control_box[i].text);
+        element_titlebar_buttons.appendChild(element_titlebar_control_box[i]);
         titlebuttonwidth += 40;
     }
     element_titlebar_buttons.style.width = titlebuttonwidth + "px";
